d453ea0fbb60a1064fedb006a9d6adb2ab3345b4
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-09-04 20:54:07 -0700

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
-mongoose.connect(`mongodb+srv://ghiasnaser:Joseph%402017@cluster0.zthxmka.mongodb.net/googlebooks?retryWrites=true&w=majority` || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+//mongoose.connect(`mongodb+srv://ghiasnaser:Joseph%402017@cluster0.zthxmka.mongodb.net/googlebooks?retryWrites=true&w=majority` || 'mongodb://localhost/googlebooks');
 module.exports = mongoose.connection;
